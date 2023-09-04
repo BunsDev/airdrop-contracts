a986211a0fd55110ac48ef89515dd789e42684ef
Layne Haber <layne.haber@gmail.com>
2023-09-03 18:03:01 -0600

    chore: update token

--- deploy/config/testnet.ts
+++ deploy/config/testnet.ts
@@ -2,7 +2,7 @@ export const TESTNET_CONFIG = {
   file: "./beneficiaries-testnet.csv",
   timelocks: {
     5: {
-      token: "0x61E0589e12395c06F6A72e5B31f803e47185FC81",
+      token: "0x9F0ec1c7eA8f317Ef7D653C18d3717f87a37B738",
       admin: "0xa2Ee8DCd2A8A3A54Cf37F6590E5108BbE502B006",
     },
   },
